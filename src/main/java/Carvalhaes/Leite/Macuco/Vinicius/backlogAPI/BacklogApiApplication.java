a7c67542147d7db839c8bc142c7f7f9d9c7f8e3c
Vinicius <macuco.vinicius@estudante.ufjf.br>
2025-04-13 17:01:17 -0300

    Criação do Projeto

---
+++ src/main/java/Carvalhaes/Leite/Macuco/Vinicius/backlogAPI/BacklogApiApplication.java
@@ -0,0 +1,13 @@
+package Carvalhaes.Leite.Macuco.Vinicius.backlogAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Carvalhaes/Leite/Macuco/Vinicius/backlogAPI/BacklogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Carvalhaes.Leite.Macuco.Vinicius.backlogAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
